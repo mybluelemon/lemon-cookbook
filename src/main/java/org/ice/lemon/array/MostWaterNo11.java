efd9119640744065ed9270f9bccdbfe6a46a30d0
yingnan <yingnan@pwrd.com>
2021-05-07 17:04:23 +0800

    reformat

--- src/main/java/org/ice/lemon/array/MostWaterNo11.java
+++ src/main/java/org/ice/lemon/array/MostWaterNo11.java
@@ -2,16 +2,17 @@ package org.ice.lemon.array;
 
 public class MostWaterNo11 {
 
-    public int maxArea(int[] height){
+    public int maxArea(int[] height) {
         //
-        int max =0 ;
+        int max = 0;
         int left = 0;
-        int right = height.length -1;
-        while(left < right){
-            max = Math.max(max, Math.abs(height[left]-height[right])* (right - left));
-            if(height[left] < height[right]){
+        int right = height.length - 1;
+        while (left < right) {
+            max = Math.max(max, Math.abs(height[left] - height[right]) * (right - left));
+            if (height[left] < height[right]) {
                 left++;
-            }{
+            }
+            {
                 right--;
             }
         }
--- src/main/java/org/ice/lemon/array/TwoSumNo1.java
+++ src/main/java/org/ice/lemon/array/TwoSumNo1.java
@@ -5,15 +5,15 @@ import java.util.HashSet;
 
 public class TwoSumNo1 {
     //return two nums index , and sum of them is target
-    public int[] twoSum(int[] nums, int target){
-        HashMap<Integer,Integer> hashMap = new HashMap();
-        for(int i = 0 ; i < nums.length; i++){
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> hashMap = new HashMap();
+        for (int i = 0; i < nums.length; i++) {
             int rest = target - nums[i];
-            if(!hashMap.containsKey(rest)){
+            if (!hashMap.containsKey(rest)) {
                 //
-                hashMap.put(nums[i],i);
-            }else{
-                return new int[]{hashMap.get(rest),i};
+                hashMap.put(nums[i], i);
+            } else {
+                return new int[]{hashMap.get(rest), i};
             }
         }
         return null;
