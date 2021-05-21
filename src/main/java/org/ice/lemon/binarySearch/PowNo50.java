ee7292eee4872e0170468208421ad2b787c7ef26
yingnan <yingnan@pwrd.com>
2021-05-21 19:34:31 +0800

    seg

--- src/main/java/org/ice/lemon/binarySearch/PowNo50.java
+++ src/main/java/org/ice/lemon/binarySearch/PowNo50.java
@@ -4,11 +4,31 @@ public class PowNo50 {
     /**
      * 很尴尬，这儿是求一个x 的n 次方
      * 如果说这是3 * 18 次方，你可以搞 n -1
+     * 先不用考虑溢出问题，可以说，二分的相乘
+     * 很尴尬，
      * @param x
      * @param n
      * @return
      */
-   public static double myPow(double x, int n){
+    public static double myPow(double x, int n) {
+        if (n == 0) {
+            return 1;
+        } else if (n == 1) {
+            return x;
+        } else if (n < 0) {
+            n = -n;
+            return 1 / myPow(x, n);
+        }
+        double tmp = myPow(x, n >> 1);
 
-   }
+        if ((n & 1) == 1) {
+            return tmp * tmp * x;
+        } else {
+            return tmp * tmp;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(myPow(2.1, 3));
+    }
 }
